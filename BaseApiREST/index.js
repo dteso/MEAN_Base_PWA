6b1ceec1f3494cab1dddbc47c6fb7094925cbdbb
David Teso <dtesodev@gmail.com>
2021-08-05 17:19:00 +0200

    public shared folder

--- BaseApiREST/index.js
+++ BaseApiREST/index.js
@@ -7,12 +7,16 @@ const { dbConnection } = require('./database/config');
 
 //CREACIÓN DEL SERVIDOR EXPRESS
 const app = express();
+const appShared = express();
 
 //CONFIGURAR CORS
 app.use(cors());
 
 //PARSEO DEL BODY
 app.use(express.json());
+
+appShared.use('/shared', express.static('shared'));
+app.use('/api', appShared);
 
 //CONEXIÓN BASE DE DATOS
 dbConnection();
@@ -22,7 +26,7 @@ app.use('/api/users', require('./routes/user.route'));
 app.use('/api/login', require('./routes/auth.route'));
 app.use('/dispatcher/alarm', require('./routes/dispatcher.route'));
 app.use('/api/notifications', require('./routes/newsletter.route'));
-// app.use('/api/newsletter', require('./routes/newsletter.route'));
+app.use('/api/upload', require('./routes/file.route'));
 
 app.listen(process.env.PORT, () => {
   console.log(`Servidor corriendo en el puerto ${process.env.PORT}`);
