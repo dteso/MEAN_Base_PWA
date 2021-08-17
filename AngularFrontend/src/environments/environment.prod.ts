aabf1dd11af75c8be44ccdde8831a626bd436d2a
David Teso <dtesodev@gmail.com>
2021-08-17 08:34:19 +0200

    enviroments

--- AngularFrontend/src/environments/environment.prod.ts
+++ AngularFrontend/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  base_url: "https://digitalophy-beta.herokuapp.com:3000/",
-  api_url: "https://digitalophy-beta.herokuapp.com:3000/api",
+  base_url: "https://digitalophy-beta.herokuapp.com/",
+  api_url: "https://digitalophy-beta.herokuapp.com/api",
   publicKey:"BAfOIbp_UDfO0Vw6rFvJ0DGHyoucjPML_w8DjsxnIgMhzCnnuwL1TSkuRwESsIo1PlU6s7C11cowbJh_HJXsLS0",
   privateKey:"5iMZSHGFKPjTjA5dnxaVSsfINHVLqBWWKxY9AEDiWb8",
   server_socket: "https://digitalophy-beta.herokuapp.com:5000"
